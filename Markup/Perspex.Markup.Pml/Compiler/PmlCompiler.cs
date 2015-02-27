bfc42c6095a89f7c2e1f4118973c1fb8c389d98c
Steven Kirk <grokys@gmail.com>
2015-02-27 18:10:05 +0100

    Revert "Try to use AdhocWorkspace to generate code."
    This reverts commit dbc2b801c66b0f3f0bc95df930f9ff298d2647a4.

--- Markup/Perspex.Markup.Pml/Compiler/PmlCompiler.cs
+++ Markup/Perspex.Markup.Pml/Compiler/PmlCompiler.cs
@@ -7,15 +7,12 @@
 namespace Perspex.Markup.Pml.Compiler
 {
     using System;
-    using Microsoft.CodeAnalysis;
-    using Microsoft.CodeAnalysis.CSharp.Formatting;
-    using Microsoft.CodeAnalysis.Formatting;
     using Perspex.Markup.Pml.Dom;
     using SF = Microsoft.CodeAnalysis.CSharp.SyntaxFactory;
 
     public class PmlCompiler
     {
-        public void Compile(Dom.Document document)
+        public void Compile(Document document)
         {
             var root = document.RootNode;
 
@@ -25,15 +22,11 @@ namespace Perspex.Markup.Pml.Compiler
             }
 
             var unit = SF.CompilationUnit().AddMembers(
-                            SF.NamespaceDeclaration(SF.ParseName(root.Type.NamespaceName)).AddMembers(
-                                SF.ClassDeclaration(SF.ParseToken(root.Type.Name))
-                                    .AddBaseListTypes(SF.SimpleBaseType(SF.ParseTypeName("Window")))));
+                SF.NamespaceDeclaration(SF.ParseName(root.Type.NamespaceName)).AddMembers(
+                    SF.ClassDeclaration(SF.ParseToken(root.Type.Name))
+                        .AddBaseListTypes(SF.SimpleBaseType(SF.ParseTypeName("Window")))));
 
-            var workspace = new AdhocWorkspace();
-            var options = workspace.Options;
-            options = options.WithChangedOption(CSharpFormattingOptions.SpaceAfterMethodCallName, true);
-
-            var output = Formatter.Format(unit, workspace, options);
+            var text = unit.ToString();
         }
     }
 }
